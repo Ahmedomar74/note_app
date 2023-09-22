ad75ab24b06c737c96a8c79f83da866a40ee4c35
Ahmedomar74 <aomar460744@gamil.com>
2023-09-22 21:35:53 +0300

    create edite note view

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:noteapp/constant/constant.dart';
+import 'package:noteapp/views/widgets/custom_bottom.dart';
 import 'package:noteapp/views/widgets/custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -8,20 +10,29 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: const [
-          SizedBox(
-            height: 50,
-          ),
-          CustomTextField(hint: 'title'),
-          SizedBox(
-            height: 17,
-          ),
-          CustomTextField(
-            hint: 'content',
-            maxlines: 5,
-          ),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: const [
+            SizedBox(
+              height: 50,
+            ),
+            CustomTextField(hint: 'title'),
+            SizedBox(
+              height: 17,
+            ),
+            CustomTextField(
+              hint: 'content',
+              maxlines: 5,
+            ),
+            SizedBox(
+              height: 60,
+            ),
+            CustomBottom(),
+            SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
       ),
     );
   }
