6b609f64371bafb8590178ac44e3f96805919bdf
Ahmedomar74 <aomar460744@gamil.com>
2023-09-23 11:48:12 +0300

    create edit body

--- lib/views/widgets/edit_view_body.dart
+++ lib/views/widgets/edit_view_body.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:noteapp/views/widgets/custom_app_bar.dart';
+import 'package:noteapp/views/widgets/custom_text_field.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Column(
+        children: const [
+          SizedBox(
+            height: 40,
+          ),
+          CustomAppBar(
+            title: 'Edit Note',
+            icon: Icons.check,
+          ),
+          CustomTextField(hint: 'title'),
+          SizedBox(
+            height: 10,
+          ),
+          CustomTextField(
+            hint: 'content',
+            maxlines: 6,
+          ),
+        ],
+      ),
+    );
   }
 }
